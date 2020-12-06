7448f20d2243d95a7e0414d0330e04b1c0b8faad
dawong <darlene.py@gmail.com>
2020-12-05 22:55:04 -0800

    Day 6

---
+++ 06-custom-customs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def get_groups(path):
+    with open(path, 'r') as fd:
+        groups = fd.read().split('\n\n')
+
+    return groups
+
+def yes_count(group):
+    return len(set(list(''.join(group.split('\n')))))
+
+groups = get_groups('06-custom-customs.txt')
+print(sum([yes_count(group) for group in groups]))
+
